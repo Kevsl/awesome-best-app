deb8139835c757c1b745735399e18a1f762bd565
Kevin Terrier <kevin.terrier@le1817.com>
2022-12-09 11:28:38 +0100

    added style

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,15 @@
-const Home = () =>{
-    return(
-        <div><h1>Home</h1> </div>
+const Home = () => {
+    return (
+        <div>
+            <div className="bg-gray-light">
+                <h1 className="text-dark-blue text-5xl text-center py-4">
+                    Meme Generator
+                </h1>
+            </div>
+            <h2 className="bg-dark-blue rounded-lg w-1/3 mx-auto my-4 text-white text-center">
+                Choose your meme
+            </h2>
+        </div>
     )
 }
 export default Home
